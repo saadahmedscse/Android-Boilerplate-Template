a8915acb30161dcd8e559b2a13a5842113be8296
saadahmedscse <saadahmedscse@gmail.com>
2023-10-09 22:17:16 +0600

    created Constants class

---
+++ app/src/main/java/com/saadahmedev/baseboilerplate/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.saadahmedev.baseboilerplate.util
+
+class Constants {
+    object AppInfo {
+        const val APP_NAME = "Base Boilerplate"
+    }
+
+    object Api {
+        const val BASE_URL = "http://192.168.0.114:8080/api/"
+    }
+}
